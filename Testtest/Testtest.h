eb37244c2662c5af2bea5fe494186c6b0f5dfc19
noobP <noobprogrammer@outlook.com>
2018-08-01 11:37:42 +0800

    上传测试

---
+++ Testtest/Testtest.h
@@ -0,0 +1,27 @@
+// Testtest.h : Testtest DLL 的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CTesttestApp
+// 有关此类实现的信息，请参阅 Testtest.cpp
+//
+
+class CTesttestApp : public CWinApp
+{
+public:
+	CTesttestApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+	DECLARE_MESSAGE_MAP()
+};
